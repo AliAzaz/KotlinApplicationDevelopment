7a67f2d203420295977e8f250374f0c91b284968
ali.azaz <ali.azaz.alam@hotmail.com>
2019-05-30 13:07:14 +0500

    Beginning Kotlin update

--- Beginning-Kotlin/Chapter1/out/production/ClassKotlin/Class7/README.md
+++ Beginning-Kotlin/Chapter1/out/production/ClassKotlin/Class7/README.md
@@ -7,3 +7,11 @@ Extend the lambda to take an argument indicating the number of sides of the dice
 If you haven't done so, fix the lambda to return 0 if the number of sides passed in is 0.
 
 Create a new variable, rollDice2, for this same lambda using the function type notation.
+
+
+##Practice Time: Extra Questions
+Why would you want to use the function type notation instead of just the lambda?
+
+Create a function gamePlay() that takes a roll of the dice as an argument and prints it out.
+
+Pass your rollDice3 function as an argument to gamePlay() to generate a dice roll every time gamePlay() is called.
--- Beginning-Kotlin/Chapter1/src/Class2/Class2.kt
+++ Beginning-Kotlin/Chapter1/src/Class2/Class2.kt
@@ -3,14 +3,15 @@ package Class2
 fun main(args: Array<String>) {
 
     for (i in 1..10) {
-        val gotFortune = getFortuneCookie()
+        val gotBirthday = getBirthday()
+        val gotFortune = getFortuneCookie(gotBirthday)
         println("Your fortune is: $gotFortune")
         if (gotFortune == "Take it easy and enjoy life!") break
     }
 
 }
 
-fun getFortuneCookie(): String {
+fun getFortuneCookie(birthday: Int): String {
 
     val fortunes = listOf(
             "You will have a great day!",
@@ -21,12 +22,14 @@ fun getFortuneCookie(): String {
             "Take it easy and enjoy life!",
             "Treasure your friends because they are your greatest fortune.")
 
-    print("Enter your birthday:")
-    val birthday = readLine()?.toIntOrNull() ?: 1
-
     return when (birthday) {
         28, 31 -> "good fortune"
         in 1..7 -> "nice fortune"
         else -> fortunes[birthday.rem(fortunes.size)]
     }
+}
+
+fun getBirthday(): Int {
+    print("Enter your birthday:")
+    return readLine()?.toIntOrNull() ?: 1
 }
